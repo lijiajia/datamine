84c26e53adcdaff930df5b7182bcc6c3ef382588
lijiajia <lijiajia_1010@126.com>
2015-04-01 10:53:16 +0800

    fix

--- word_segmentation_HMM.py
+++ word_segmentation_HMM.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from jieba import finalseg
+from jieba.finalseg.prob_start import P
 
+
+print P
+res = finalseg.cut('我是一个好学生,活着真不容易,快干活泼')
+print ','.join(res)
